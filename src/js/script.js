137d00d035dc7d4d807004745135ebe5cae37cc6
hsiangcoco <f1346790@gmail.com>
2025-01-06 11:50:37 +0800

    0106 11:50

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,14 @@
-$(function(){
+$(function () {
+    $('.slideText').textslider({
+        direction: 'scrollUp', // 捲動方向: scrollUp向上, scrollDown向下
+        scrollNum: 1, // 一次捲動幾個元素
+        scrollSpeed: 800, // 捲動速度(ms)
+        pause: 3000 // 停頓時間(ms)
+    });
 })
+
+
+
+
+
+
